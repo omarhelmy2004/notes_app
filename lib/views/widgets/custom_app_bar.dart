f6fb0f291ac7ae5f45c350e75cd9692442898d95
omarhelmy2004 <omarhelmy2004@hotmail.com>
2024-07-11 07:42:32 +0300

    Edit note done

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_search_icon.dart';
+import 'package:notes_app/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.title, required this.icon});
+  const CustomAppBar(
+      {super.key, required this.title, required this.icon, this.onPressed});
   final String title;
   final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -14,7 +16,8 @@ class CustomAppBar extends StatelessWidget {
           title,
           style: const TextStyle(fontSize: 28),
         ),
-        CustomSearchIcon(
+        CustomIcon(
+          onPressed: onPressed,
           icon: icon,
         ),
       ],
--- lib/views/widgets/custom_icon.dart
+++ lib/views/widgets/custom_icon.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key, required this.icon});
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key, required this.icon, this.onPressed});
   final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,9 +13,12 @@ class CustomSearchIcon extends StatelessWidget {
           color: Colors.white.withOpacity(.1),
           borderRadius: BorderRadius.circular(16)),
       child: Center(
-          child: Icon(
-        icon,
-        size: 28,
+          child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          size: 28,
+        ),
       )),
     );
   }
